751ba2a207ddf332650c2ac388554fb8a14e55d3
Hareesh7981 <hh785713@gmail.com>
2026-07-14 20:35:13 +0530

    Update app.py

--- 4. Project Development/app.py
+++ 4. Project Development/app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import joblib
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# Load trained model
+model = joblib.load("credit_card_model.pkl")
 
-if __name__ == '__main__':
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    income = float(request.form["income"])
+    employment_days = float(request.form["employment_days"])
+
+    data = pd.DataFrame({
+        "AMT_INCOME_TOTAL":[income],
+        "DAYS_EMPLOYED":[employment_days]
+    })
+
+    prediction = model.predict(data)
+
+    if prediction[0] == 1:
+        result = "✅ Credit Card Approved"
+    else:
+        result = "❌ Credit Card Rejected"
+
+    return render_template("index.html", prediction=result)
+
+if __name__ == "__main__":
     app.run(debug=True)
