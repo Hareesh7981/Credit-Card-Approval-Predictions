4025d8567d6fffff384157039528f422d209c5a6
Hareesh7981 <hh785713@gmail.com>
2026-07-19 21:17:32 +0530

    Update app.py

--- 4. Project Development/app.py
+++ 4. Project Development/app.py
@@ -1,3 +1,6 @@
+income = float(request.form["income"])
+employment_days = float(request.form["employment_days"])
+
 data = pd.DataFrame({
     "AMT_INCOME_TOTAL": [income],
     "YEARS_EMPLOYED": [employment_days]
